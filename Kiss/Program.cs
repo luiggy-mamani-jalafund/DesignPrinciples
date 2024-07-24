18ba2daaa63d923c8de4099a2f5e37fe730ead85
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-07-23 22:15:32 -0400

    feat: example for kiss principle; guessing game game runner

---
+++ Kiss/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kiss;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GuessingNumberGame guessingNumberGame = new GuessingNumberGame(0, 100);
+        guessingNumberGame.StartGame();
+    }
+}
